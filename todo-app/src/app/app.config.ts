77d144f30cb072476b8b33fe8be255d7312dc705
Shahzod Kakhkhorov <57046438+ShahzodK@users.noreply.github.com>
2024-10-24 13:23:26 +0500

    create project, download packages

---
+++ todo-app/src/app/app.config.ts
@@ -0,0 +1,32 @@
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return  new  TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+export const provideTranslation = () => ({
+  defaultLanguage: 'en',
+  loader: {
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory,
+    deps: [HttpClient],
+  },
+});
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideStore(), 
+    provideAnimationsAsync(),
+    importProvidersFrom(TranslateModule.forRoot(provideTranslation())),
+  ]
+};
